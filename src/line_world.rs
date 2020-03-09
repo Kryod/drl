6e962a8ab26b41859610c617d6de64650b7333e3
Jerrain Yohann <kryod@users.noreply.github.com>
2020-03-09 14:32:25 +0100

    line_world done

--- src/line_world.rs
+++ src/line_world.rs
@@ -1,4 +1,4 @@
-use ndarray::{arr1, Array3};
+use ndarray::{arr1, Array1, Array3};
 
 //S = np.array([0, 1, 2, 3, 4])
 //A = np.array([0, 1])  # 0: left, 1 : right
@@ -12,14 +12,19 @@ use ndarray::{arr1, Array3};
 
 //R[1, 0, 0] = -1.0
 //R[3, 1, 4] = 1.0
-fn init() {
+fn init() -> (Array1<usize>, Array1<i32>, Array1<i32>, Array3<f32>, Array3<f32>){
     let S = arr1(&[0,1,2,3,4]);
     let A = arr1(&[0,1]);
     let T = arr1(&[0,4]);
-    let P = Array3::<f32>::zeros((S.shape()[0], A.shape()[0], S.shape()[0]));
-    let R = Array3::<f32>::zeros((S.shape()[0], A.shape()[0], S.shape()[0]));
+    let mut P = Array3::<f32>::zeros((S.shape()[0], A.shape()[0], S.shape()[0]));
+    let mut R = Array3::<f32>::zeros((S.shape()[0], A.shape()[0], S.shape()[0]));
 
-    //for s in S {
+    for s in S.iter() {
+        P[(*s, 0 as usize, s-1 as usize)] = -1.0;
+        P[(*s, 0 as usize, s+1 as usize)] = 1.0;
+    }
+    R[(1, 0, 0)] = -1.0;
+    R[(3, 1, 4)] = 1.0;
 
-    //}
+    (S, A, T, P, R)
 }
