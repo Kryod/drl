71fbf091a7887a9b44c28c4d249435788109701a
Axel Cocat <ax.cocat@gmail.com>
2020-03-09 17:11:28 +0100

    Iterative policy evaluation

---
+++ src/algorithms/mod.rs
@@ -0,0 +1,3 @@
+pub mod iterative_policy_evaluation;
+
+pub use iterative_policy_evaluation::iterative_policy_evaluation;
--- src/policies.rs
+++ src/policies.rs
@@ -5,5 +5,5 @@ use ndarray::Array2;
 //    return np.ones((state_size, action_size)) / action_size
 
 pub fn create_random_uniform_policy(state_size: usize, action_size: usize) -> Array2<f32> {
-    Array2::<f32>::ones((state_size,action_size)) / action_size as f32
+    Array2::<f32>::ones((state_size, action_size)) / action_size as f32
 }
